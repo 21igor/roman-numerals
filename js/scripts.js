ec4fba7360f11cf4c4757944891bd69d84651a9f
Igor Seleznov and Rony Nasr <igor@>
2016-06-09 10:02:27 -0700

    Added complete back-end logic

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+
+var translate = function(number) {
+  var values = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+  var result = "";
+  var remainder = number;
+
+  values.forEach(function(value){
+    var count = parseInt(remainder/value);
+    result = result + toRoman(value,count)
+    remainder = remainder%value;
+
+  });
+  return result;
+}
+
+var toRoman = function (value,count) {
+  var values = [[1000,"M"],[900,"CM"],[500,"D"],[400,"CD"],[100,"C"],
+              [90,"XC"],[50,"L"],[40,"XL"],[10,"X"],[9,"IX"],[5,"V"],[4,"IV"],[1,"I"]]
+  var res = "";
+  values.forEach(function(V,index) {
+    for (var i = 0; i < count; i++) {
+      if(value === V[0]){
+        res = res + V[1];
+      }
+    }
+  });
+  return res;
+}
+
+//Front end logic
+$(function(){
+  $("form#roman-numerals").submit(function(event) {
+    event.preventDefault();
+    var decNumber = parseInt($("input#dec-number").val());
+//    console.log(decNumber);
+    if (decNumber > 3999 || decNumber < 0) {
+      alert("PLease enter a number between 0 and 3999");
+    }else {
+      $("#result").text(translate(decNumber));
+    }
+
+  });
+});
